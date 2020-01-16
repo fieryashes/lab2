b8aaf6144021001f3da53ca90da414ff7f3301f4
Alisha Sehgal <alishaseh@gmail.com>
2020-01-16 13:39:45 -0800

    stretch Goal - added style
    - move image after click
    - background color only changes as an under border in order to allow a shelf-effect when the image moves

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -6,4 +6,25 @@ function initializePage() {
     $('#testjs').click(function(e) {
             $('.jumbotron h1').text("Javascript is connected");
             });
+    $("a.thumbnail").click(projectClick);
+    
+ //    function projectClick(e) { 
+ //    // prevent the page from reloading      
+ //    e.preventDefault();
+ //    // In an event handler, $(this) refers to      
+ //    // the object that triggered the event      
+ //    $(this).css("background-color", "#7fff00");
+	// }
+
+}
+
+function projectClick(e) { 
+    // prevent the page from reloading      
+    e.preventDefault();
+    // In an event handler, $(this) refers to      
+    // the object that triggered the event      
+    $(this).css("background-color", "#7fff00");
+    $(this).css("width", "525px");
+    $(this).css("padding", "20px");
+    
 }
